fa79613c48618c03ae9139b7fee84e6ea0aa8b81
maher <gomemahero.com>
2022-08-26 00:51:38 +0300

    redepyy

--- utils/serverUrl.js
+++ utils/serverUrl.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 // next-social-app-tau.vercel.app
 export const server = dev
   ? "http://localhost:3000"
-  : "http://localhost:3000";
+  : "https://next-social-nervh8s0z-maher9911133-gmailcom.vercel.app/";
